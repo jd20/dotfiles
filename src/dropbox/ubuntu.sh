2b96ae265d61bd18632d7f1ed6fd89741bb581ea
Jason Douglas <jasond@me.com>
2017-04-11 02:52:57 -0700

    Use xdg-open to install dropbox

--- src/dropbox/ubuntu.sh
+++ src/dropbox/ubuntu.sh
@@ -11,18 +11,18 @@ install_dropbox() {
     # Download and install Dropbox
 
     if ! is_dropbox_configured; then
-        install_deb "Dropbox", "https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb"
+
+        wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb -o /tmp/dropbox_2015.10.28_amd64.deb
+        xdg-open /tmp/dropbox_2015.10.28_amd64.deb
     fi
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     # Wait until the Dropbox folder is present
 
-    execute \
-        "until is_dropbox_configured; do \
-            sleep 2; \
-         done" \
-        "Install Dropbox"
+    until is_dropbox_configured; do
+        sleep 2
+    done
 
 }
 
@@ -34,4 +34,4 @@ is_dropbox_configured() {
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-install_dropbox
+execute "install_dropbox" "Install Dropbox"
