8f3e4e2e23dd00b6bdc762bcb6360e7867cbb720
Jason Douglas <jasond@me.com>
2017-04-10 18:01:32 -0700

    Simplify virtual environment loading for Ruby / Python

--- src/python/path.zsh
+++ src/python/path.zsh
@@ -1,25 +1,7 @@
 #!/bin/sh
 
-__pyenv_started=0
-
-__pyenv_init() {
-  test $__pyenv_started = 0 && {
-    eval "$(command pyenv init -)"
-    __pyenv_started=1
-  }
-}
-
-pyenv() {
-  __pyenv_init
-  command pyenv "$@"
-}
-
-python() {
-  __pyenv_init
-  command python "$@"
-}
-
-pip() {
-  __pyenv_init
-  command pip "$@"
-}
+if [ -z $__pyenv_started ]; then
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
+  __pyenv_started="x"
+fi
