0d10c32a6f5819e499ee60f3ca0516d2afbc2f6a
Jason Douglas <jasond@me.com>
2017-04-12 21:50:56 -0700

    Install Chrome instead of Chromium

--- src/dropbox/ubuntu.sh
+++ src/dropbox/ubuntu.sh
@@ -12,7 +12,7 @@ install_dropbox() {
 
     if ! is_dropbox_configured; then
 
-        add_to_source_list "[arch=i386,amd64] http://linux.dropbox.com/ubuntu xenial main" "dropbox.list"
+        add_to_source_list "[arch=amd64] http://linux.dropbox.com/ubuntu stable main" "dropbox.list"
         sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
         sudo apt update -y
         sudo apt install -y dropbox python-gpgme
--- src/ubuntu/apps.sh
+++ src/ubuntu/apps.sh
@@ -11,14 +11,17 @@ install_apps() {
     #install_package "debconf-utils" "debconf-utils"
     #add_ppa "webupd8team/java"
     #add_ppa "paolorotolo/android-studio"
-    #apt_update
-    install_package "libindicator / libappindicator" "libindicator7 libappindicator1"
 
+    add_key "https://dl-ssl.google.com/linux/linux_signing_key.pub"
+    add_to_source_list "[arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" "google-chrome.list"
+    apt_update
+
+    install_package "libindicator / libappindicator" "libindicator7 libappindicator1"
     #install_package "Ubuntu Make" "ubuntu-make"
     #execute "umake android --accept-license" "Android Studio"
     install_package "Build Essential" "build-essential"
     install_package "GnuPG archive keys" "debian-archive-keyring"
-    install_package "Chromium" "chromium-browser"
+    install_package "Chrome" "google-chrome-stable"
     install_package "cURL" "curl"
     #execute "umake go \"$HOME/.local/share/umake/go/go-lang\"" "Go"
     install_package "NodeJS" "nodejs"
